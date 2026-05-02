d6d5e00a549c437d038d783947db693933a1583c
khaled-Gui <37468405+khaled-Gui@users.noreply.github.com>
2026-05-02 06:17:38 +0100

    Refactor imports in hero.tsx
    Removed unused imports and added Badge component.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,8 @@
-
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ArrowUpRight, CirclePlay } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import React from "react";
-import LogoCloud from "./logo-cloud";
+
 
 const Hero = () => {
   return (
